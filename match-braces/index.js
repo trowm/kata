b04837c67f695b56570b2e6cea83be0c020a28fc
Matt Trow <m.trow@elsevier.com>
2022-07-30 16:47:37 +0100

    all the stuff

--- match-braces/index.js
+++ match-braces/index.js
@@ -28,13 +28,13 @@ const validStartBrackets = ["[", "{", "("];
 function areBracketsValid(input) {
   /**
    * Some super basic checks - is it a string, is it empty?
-   * We bail early here.
-   * We also don't _combine_ the checks into one if statement with an OR (||),
-   * because the first check is to see if we have a string, the second is the string empty.
-   * We _only_ accept a typeof string, so combining them makes no sense, we don't _know_
-   * if it's a string, until we've checked it.
+   * I bail early here.
+   * I also don't _combine_ the checks into one if statement with an OR (||),
+   * because the first check is to see if I have a string, the second is the string empty.
+   * I _only_ accept a typeof string, so combining them makes no sense, I don't _know_
+   * if it's a string, until I've checked it.
    *
-   * Could we add a regex check here too? - that our string only contains valid brackets?
+   * Could I add a regex check here too? - that the string only contains valid brackets?
    * Sure - that's also a quick way to bail early.
    */
   if (typeof input !== "string") return false;
@@ -47,24 +47,23 @@ function areBracketsValid(input) {
   const inputToArray = input.split("");
   if (!validStartBrackets.includes(inputToArray[0])) return false;
 
-  let isValid = true; // This is used as our return var
+  let isValid = true; // This is used as the return var
   let bracketCount = 0; // This is used to increment & decrement as brackets are opened or closed
 
   /**
-   * We loop through our input we converted to an array
+   * I loop through the input that was converted to an array
    */
   for (let i = 0; i < inputToArray.length; ++i) {
     /**
-     * In our switch, we only check the opening brackets against a valid list of what can follow one, never closing brackets.
+     * In the switch, I only check the opening brackets against a valid list of what can follow one, never closing brackets.
      *
      * Why?:
-     * 1. we've already checked our very first bracket in the array for validity, before we hit the loop. It should be an OPENING bracket, else it isn't valid.
-     * 2. For those opening brackets, we're checking whether the _NEXT_ bracket is valid, not the _current_ one. That's the important part of this solution. The other part, uses the bracketCount var.
+     * 1. I've already checked the very first bracket in the array for validity, before I hit the loop. It should be an OPENING bracket, else it isn't valid.
+     * 2. For those opening brackets, I'm checking whether the _NEXT_ bracket is valid, not the _current_ one. That's one important part of this solution. The other part uses the bracketCount var.
      *
-     * So, for each open bracket we increment the bracketCount, for each closing, we decrement it.
+     * So, for each open bracket I increment the bracketCount, for each closing, I decrement it.
      *
-     * Using a switch is handy, although in this case, it seems more verbose, we have an easy way to break out of a switch,
-     * so we don't have to do any more checks - we found our case, our "match"
+     * Using a switch is handy, although in this case, it seems more verbose, but as there is an easy way to break out of a switch, I don't need to keep traversing through the other cases - I love a good switch! 
      */
     switch (inputToArray[i]) {
       case "[":
@@ -90,26 +89,26 @@ function areBracketsValid(input) {
         break;
       default:
         /**
-         * Why is our default false? - because if _none_ of the above cases are found, then it isn't a bracket.
+         * Why is the default false? - because if _none_ of the above cases are found, then it isn't a bracket.
          */
         isValid = false;
         break;
     }
     /**
      * Handy thing about `for` loops - you can break out of them.
-     * The moment we've got an isValid false boolean out of our switch statement, we bail.
+     * The moment isValid is false, I bail out.
      */
     if (isValid === false) break;
   }
 
   /**
-   * Our final check - if bracketCount isn't zero, it means we're missing a matching closing brace.
-   * In a way, this is the "Secret Sauce" to my solution, in our switch, we've checked the validity of the _next_ bracket
-   * but by using a bracketCount, we don't have to bother with checking validity for closing everything.
+   * The final check - if bracketCount isn't zero, it means I'm missing a matching closing brace.
+   * In a way, this is the "Secret Sauce" to my solution, in the switch, I've checked the validity of the _next_ bracket
+   * but by using a bracketCount, I don't have to bother with checking validity for closing everything.
    * If it isn't zero, then it isn't closed.
-   * This allows us to not bother checking something like `(((((` to see if it's valid during our loop through all of our brackets, instead, we use a count.
+   * This allows me to not bother checking something like `(((((` to see if it's valid during the loop through all of the brackets, instead, I use a count.
    * It simplifies things considerably.
-   * Think about it - we're only really checking if the NEXT bracket is valid, we don't care whether we're closing stuff or not. Our count does all that for us = winning.
+   * Think about it - this is only really checking if the NEXT bracket is valid, I don't care whether I'm closing stuff or not. The count does all that for me = winning.
    */
   if (bracketCount !== 0) isValid = false;
 
